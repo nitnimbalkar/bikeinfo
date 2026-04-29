4f479250dc39d1d34f031185b8a27d2983d166fc
Nitin Nimbalkar <nitin21kn@gmail.com>
2026-04-29 15:37:04 +0530

    Update app.js
    setting project

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express=require('express');
 const app=express()
+const cookieParser = require('cookie-parser');
+const path=require('path');
+
+//Middlewares
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.set('view engine','ejs');
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname,'public')));
+
 
 
 app.get('/',(req,res)=>{
