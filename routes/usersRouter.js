19b8ad57e8aa333c51473a58e66d5a7fba8533c9
Nitin Nimbalkar <nitin21kn@gmail.com>
2026-05-01 16:07:52 +0530

    adding bcrypt and jwt

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,29 +1,42 @@
 const express=require('express');
 const router = express.Router();
 const userModel=require("../models/userModel");
+const bcrypt=require("bcrypt");
+const jwt=require("jsonwebtoken"); 
+
 
 router.get("/",(req,res)=>{
     res.render("signup");
 })
 
-router.post("/register",async(req,res)=>{
+router.post("/register",(req,res)=>{
     try{
         let {username,firstname,lastname,email,age,city,password} = req.body;
-        let user = await userModel.create({
-        username,
-        firstname,
-        lastname,
-        email,
-        age,
-        city,
-        password,
+
+        bcrypt.genSalt(10, function(err,salt){
+            bcrypt.hash(password,salt,async function(err,hash){
+                if (err) return res.send(err.message);
+                else{
+                    let user = await userModel.create({
+                        username,
+                        firstname,  
+                        lastname, 
+                        email,
+                        age,
+                        city,
+                        password:hash,
+                        }); 
+                        let token=jwt.sign({email,id:user._id},"jwtsecreat");
+                        res.cookie("token",token);
+                        res.send("User Created Successfully");
+                }
+
+            });
         });
-        res.send(user);
     }catch(err) {
         console.log(err.message); 
     }
 
 })
-
 
 module.exports=router;
