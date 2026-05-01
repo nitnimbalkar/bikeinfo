8d695b19f02fd14d752f2e67e5d144011bf02179
Nitin Nimbalkar <nitin21kn@gmail.com>
2026-05-01 17:56:58 +0530

    added logged in method

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,9 +3,14 @@ const bcrypt=require("bcrypt");
 const jwt=require("jsonwebtoken"); 
 const {generateToken }=require("../utils/generateToken");
 
-module.exports.registerUser = (req,res)=>{
+module.exports.registerUser = async (req,res)=>{
     try{
         let {username,firstname,lastname,email,age,city,password} = req.body;
+
+        let user =await userModel.findOne({email:email})
+        
+        if(user)
+            return res.status(401).send("User Already Registred");
 
         bcrypt.genSalt(10, function(err,salt){
             bcrypt.hash(password,salt,async function(err,hash){
@@ -31,4 +36,21 @@ module.exports.registerUser = (req,res)=>{
         console.log(err.message); 
     }
 
+};
+
+module.exports.loginUser =  async (req,res)=>{
+    let {email,password} = req.body;
+
+    let user = await userModel.findOne({ email:email });
+    if(!user) return res.status(400).send("Email or Password Incorrect");
+
+    bcrypt.compare(password, user.password, function (err, result){
+        if(result==true){
+            let token = generateToken(user);
+            res.cookie("token",token);
+            res.send("You are logged in");
+        }else{
+            res.status(400).send("Email or Password Incorrect");
+        }
+    })
 };
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router = express.Router();
-const { registerUser } = require('../controllers/authController');
+const { registerUser, loginUser } = require('../controllers/authController');
 
 
 router.get("/",(req,res)=>{
@@ -8,5 +8,7 @@ router.get("/",(req,res)=>{
 })
 
 router.post("/register",registerUser);
+
+router.post("/login",loginUser);
 
 module.exports=router;
