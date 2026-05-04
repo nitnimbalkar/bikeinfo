0ee0217443d3fc1c1029fca29164a861e41bc4bd
Nitin Nimbalkar <nitin21kn@gmail.com>
2026-05-04 20:55:12 +0530

    tuning bike add route

--- routes/bikesRouter.js
+++ routes/bikesRouter.js
@@ -8,12 +8,42 @@ router.get("/",async(req,res)=>{
 })
 
 router.post("/create", async(req,res)=>{
-    let {brand,name,isAvailableIndia,engineCapacity,transmission}=req.body;
+    let {brand,name,engineCapacity,transmission,isAvailableIndia,bProfileImg,
+    bikeImgs,bReview,mileage,weight,fuelTankCapacity,seatHeight,maxPower,
+    maxTorque,noOfCylinder,compressRatio,emissionStandard,fuelType,qShifter,
+    mConnect,gps,tracControl,cControl,serCost,fService,sService,tService,
+    sInterval,vWarentyKm}=req.body;
+    
     let addBike= await bikeModel.create({
         brand,
         name,
         engineCapacity,
         transmission,
+        isAvailableIndia,
+        bProfileImg,
+        bikeImgs,
+        bReview,
+        mileage,
+        weight,
+        fuelTankCapacity,
+        seatHeight,
+        maxPower,
+        maxTorque,
+        noOfCylinder,
+        compressRatio,
+        emissionStandard,
+        fuelType,
+        qShifter,
+        mConnect,
+        gps,
+        tracControl,
+        cControl,
+        serCost,
+        fService,
+        sService,
+        tService,
+        sInterval,
+        vWarentyKm,
 
     });
     res.status(201).send("Bike Added");
