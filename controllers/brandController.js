cb196fd7d34f63c81c5dc650a5c6726f6175c5c7
nitin <nitin21kn@gmail.com>
2026-05-26 23:44:36 +0530

    addbrand

---
+++ controllers/brandController.js
@@ -0,0 +1,38 @@
+const bikeModel=require("../models/brandModel");
+const isLoggedIn=require("../middlewares/isLoggedIn");
+
+module.exports.addbrand=async(req,res)=>{
+    let {brandName,brandOrigin,brandImage,brandNews,bikes,brandNewsImage}=req.body;
+    try{
+        if (!brandName || !brandOrigin) {
+
+            return res.status(400).json({
+                success: false,
+                message: "Please fill all required fields"
+            });
+
+            }
+            let addbrand=await bikeModel.create({
+                brandName,
+                brandOrigin,
+                brandImage,
+                brandNews,
+                bikes,
+                brandNewsImage
+            });
+            return res.status(201).json({
+                success: true,
+                message: "Brand Added Successfully",
+                bike: addBike
+            });
+    }catch (error) {
+
+        console.log(error);
+
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+
+    }
+}
