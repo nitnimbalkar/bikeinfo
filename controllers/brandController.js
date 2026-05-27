52bfb0ca326ba26044b7f48accbbe97d5b46cc65
nitin <nitin21kn@gmail.com>
2026-05-27 23:59:03 +0530

    brand add page fully functional

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,38 +1,43 @@
-const bikeModel=require("../models/brandModel");
-const isLoggedIn=require("../middlewares/isLoggedIn");
+const bikeModel = require("../models/brandModel");
+const isLoggedIn = require("../middlewares/isLoggedIn");
 
-module.exports.addbrand=async(req,res)=>{
-    let {brandName,brandOrigin,brandImage,brandNews,bikes,brandNewsImage}=req.body;
-    try{
-        if (!brandName || !brandOrigin) {
+module.exports.addbrand = async (req, res) => {
+  try {
+    const { brandName, brandOrigin } = req.body;
 
-            return res.status(400).json({
-                success: false,
-                message: "Please fill all required fields"
-            });
-
-            }
-            let addbrand=await bikeModel.create({
-                brandName,
-                brandOrigin,
-                brandImage,
-                brandNews,
-                bikes,
-                brandNewsImage
-            });
-            return res.status(201).json({
-                success: true,
-                message: "Brand Added Successfully",
-                bike: addbrand
-            });
-    }catch (error) {
-
-        console.log(error);
-
-        return res.status(500).json({
-            success: false,
-            message: "Internal Server Error"
-        });
-
+    if (!brandName || !brandOrigin) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill all required fields"
+      });
     }
-}
+
+    const file = req.file;
+
+    let brandImage = "";
+
+    if (file) {
+      brandImage = `data:${file.mimetype};base64,${file.buffer.toString("base64")}`;
+    }
+
+    const addbrand = await bikeModel.create({
+      brandName,
+      brandOrigin,
+      brandImage
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Brand Added Successfully",
+      brand: addbrand
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error"
+    });
+  }
+};
--- routes/brandsRouter.js
+++ routes/brandsRouter.js
@@ -2,13 +2,16 @@ const express=require('express');
 const router = express.Router();
 const userModel=require("../models/userModel");
 const { addbrand } = require('../controllers/brandController');
+const upload = require("../config/multerConfig");
 
 router.get("/",async(req,res)=>{
     let user= await userModel.findOne({email,user:user._id});
     res.render("signup",{user});
 })
 
-router.post("/create",addbrand);
+//Brand add Route
+router.post("/add-brand",upload.single("brandImage"),addbrand);
+
 
 router.get("/addbrand",(req,res)=>{
     res.render("addBrand.ejs");
